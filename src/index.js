6d9a25e79509c107349c51bbc12da2e114613068
SidraOsman <osmansidra4@gmail.com>
2023-10-13 08:49:38 +0300

    Done with Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit' , (e)=>{
+    e.preventDefault()
+    createToDo(e.target.new-task-description.value)
+    form.reset()
+  })
 });
+
+ 
+function createToDo(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click' , deleteTask)
+  btn.textContent = 'x'
+  li.textContent = ' ${todo} '
+  p.appendChild(btn)
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
